c0bb60c9007b57b4d2e1bbdcb31b2bba6d64d945
João Seixas <joaorodrigues.seixas@gmail.com>
2025-07-17 00:48:51 -0300

    add routes to member

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   resources :companies, only: [:index, :show, :create] do
-    resources :employees, only: [:index, :create]
+    resources :employees, only: [:show, :destroy] do
+      member do
+        put :assign_manager
+        get :peers
+        get :subordinates
+        get :second_level_subordinates
+      end
+    end
   end
-
   resources :employees, only: [:show, :destroy]
 end
