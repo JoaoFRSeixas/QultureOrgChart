b99b3e02c91b5394a16c7fa1f59b3e8b49f8122e
João Seixas <joaorodrigues.seixas@gmail.com>
2025-07-17 00:43:25 -0300

    create controllers and routes

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,26 @@
 class CompaniesController < ApplicationController
+  def index
+    companies = Company.all
+    render json: companies, status: :ok
+  end
+
+  def show
+    company = Company.find(params[:id])
+    render json: company, status: :ok
+  end
+
+  def create
+    company = Company.new(company_params)
+    if company.save
+      render json: company, status: :created
+    else
+      render json: { errors: company.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name)
+  end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,38 @@
 class EmployeesController < ApplicationController
+  before_action :set_company, only: [:index, :create]
+
+  def index
+    employees = @company.employees
+    render json: employees, status: :ok
+  end
+
+  def create
+    employee = @company.employees.new(employee_params)
+    if employee.save
+      render json: employee, status: :created
+    else
+      render json: { errors: employee.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    employee = Employee.find(params[:id])
+    render json: employee, status: :ok
+  end
+
+  def destroy
+    employee = Employee.find(params[:id])
+    employee.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_company
+    @company = Company.find(params[:company_id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:name, :email, :picture, :manager_id)
+  end
 end
